608d91936438c9f263b8031efc23d1f5414a8c94
arshdev16 <arshisha2516@gmail.com>
2021-09-06 19:23:40 +0530

    displaying live news

--- src/News.js
+++ src/News.js
@@ -2,36 +2,36 @@ import React, { Component } from 'react';
 import Newsitem from './Newsitem.js'
 
 export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      articles: [],
+      loading: false
+    }
+  }
   
+  async componentDidMount(){
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ffe09bf15bcc42d9a95d847238847551";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({articles: parsedData.articles})
+  }
  render() {
     return(
       <>
       <div className="container">
        <h3 className="my-4">Today's HeadLines</h3>
-       <div className="row">
-        <div className="col">
-         <Newsitem imgUrl="https://cdn.pixabay.com/photo/2021/04/29/05/03/desert-6215514_1280.jpg" description="bulk text to fill up the space" />
+       <div className="row d-flex">
+        {this.state.articles.map((element)=>{
+         return <div className="col-md-4 justify-content-center" key="url">
+         <Newsitem title={element.title} imgUrl={element.urlToImage} description={element.description} newsUrl={element.url} />
         </div>
-        <div className="col">
-         <Newsitem imgUrl="https://cdn.pixabay.com/photo/2021/04/29/05/03/desert-6215514_1280.jpg" description="bulk text to fill up the space" />
+       })}
+        
         </div>
-        <div className="col"> 
-        <Newsitem imgUrl="https://cdn.pixabay.com/photo/2021/04/29/05/03/desert-6215514_1280.jpg" description="bulk text to fill up the space"/>
         </div>
-      </div>
-      <div className="row">
-        <div className="col">
-         <Newsitem imgUrl="https://cdn.pixabay.com/photo/2021/04/29/05/03/desert-6215514_1280.jpg" description="bulk text to fill up the space"/>
-        </div>
-        <div className="col">
-         <Newsitem imgUrl="https://cdn.pixabay.com/photo/2021/04/29/05/03/desert-6215514_1280.jpg" description="bulk text to fill up the space"/>
-        </div>
-        <div className="col"> 
-        <Newsitem imgUrl="https://cdn.pixabay.com/photo/2021/04/29/05/03/desert-6215514_1280.jpg" description="bulk text to fill up the space"/>
-        </div>
-      </div>
-      </div> 
-      < />
+        <div className="container"></div>
+      </>
 
     )
   }
