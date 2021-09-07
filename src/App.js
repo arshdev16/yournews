1a5da3fb2a52b3f35ccd36641730632498adea6b
arshdev16 <arshisha2516@gmail.com>
2021-09-07 22:21:44 +0530

    somewhat routed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,46 @@ import React, { Component } from 'react';
 import Navbar from './Navbar.js';
 import News from './News.js';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 export default class App extends Component {
 
   render() {
     return(
       <>
-        <Navbar />
-        <News country="in" category="technology" newsPerPage={12}/>
-      </>
+      <Router>
+       <Navbar />
+        <Switch>
+          <Route exact path="/buisness">
+            <News category="business"/>
+          </Route>
+          <Route exact path="/entertainment">
+            <News category="entertainment"/>
+          </Route>
+          <Route exact path="/general">
+            <News category="general"/>
+          </Route>
+          <Route exact path="/health">
+            <News category="health"/>
+          </Route>
+          <Route exact path="/science">
+            <News category="science"/>
+          </Route>
+          <Route exact path="/sports">
+            <News category="sports"/>
+          </Route>
+          <Route exact path="/technology">
+            <News category="technology"/>
+          </Route>
+        </Switch>
+    </Router>
+  </>
     )
   }
 }
